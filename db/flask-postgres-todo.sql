06d13d4617eeb1a1b00ab2b3df9618cd672197f7
Administrator <admin@example.com>
2021-03-05 09:13:01 +0000

    Update db/flask-postgres-todo.sql, flask-postgres-todo.sql files

---
+++ db/flask-postgres-todo.sql
@@ -0,0 +1,20 @@
+-- Adminer 4.8.0 PostgreSQL 11.11 dump
+
+DROP TABLE IF EXISTS "todo";
+DROP SEQUENCE IF EXISTS todo_id_seq;
+CREATE SEQUENCE todo_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+
+CREATE TABLE "public"."todo" (
+    "id" integer DEFAULT nextval('todo_id_seq') NOT NULL,
+    "title" character varying(100),
+    "complete" boolean,
+    CONSTRAINT "todo_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
+
+INSERT INTO "todo" ("id", "title", "complete") VALUES
+(1,	'cccccccccccccccc',	'0'),
+(2,	'asdddddddd',	'1'),
+(4,	'asdddddddddddd',	'0'),
+(3,	'計畫工讀生',	'1');
+
+-- 2021-03-05 09:12:13.712878+00
